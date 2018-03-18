aa9c7421e88379f6182d3f409efde38c49cd4f59
ltnguyen26 <30305364+ltnguyen26@users.noreply.github.com>
2018-03-18 16:32:35 -0500

    FADING image

--- app/src/main/java/com/example/thuylinhnguyen/ipancreme/MainActivity.java
+++ app/src/main/java/com/example/thuylinhnguyen/ipancreme/MainActivity.java
@@ -5,9 +5,32 @@ import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
+    public void fade(View view) {
+
+        ImageView brushes = (ImageView) findViewById(R.id.brushes);
+        ImageView birds = (ImageView) findViewById(R.id.birds);
+        if(brushes.getAlpha()==1) {
+            brushes.animate().alpha(0f).setDuration(2000);
+            birds.animate().alpha(1f).setDuration(2000);
+
+        }
+        else{
+            brushes.animate().alpha(1f).setDuration(2000);
+            birds.animate().alpha(0f).setDuration(2000);
+        }
+    }
+
+//    public void rfade(View view) {
+//        ImageView brushes = (ImageView) findViewById(R.id.brushes);
+//        ImageView birds = (ImageView) findViewById(R.id.birds);
+//        brushes.animate().alpha(1f).setDuration(2000);
+//        birds.animate().alpha(0f).setDuration(2000);
+//    }
 
 
     @Override
@@ -16,12 +39,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void sendMessage(View view) {
-        // Do something in response to button
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    }
+//    public void sendMessage(View view) {
+//        // Do something in response to button
+//        Intent intent = new Intent(this, DisplayMessageActivity.class);
+//        EditText editText = (EditText) findViewById(R.id.editText);
+//        String message = editText.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
+//        startActivity(intent);
+//    }
 }
